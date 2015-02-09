1ab51d589eb6913edd6dca06e6366b600a44b51c
stephjin <stephanie.k.jin@gmail.com>
2015-02-08 21:54:23 -0800

    Create form for new tasks.

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -4,9 +4,31 @@ Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
   // This code only runs on the client
+
+  // Pass data into temapltes from JS by defining helpers
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  // Call function when submit is hit on form
+  // key = describe the event to listen for (listening for submit event on any element matching selector .new-task)
+  // value = event handlers that are called when event happens
+  Template.body.events({
+    "submit .new-task": function (event) {
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
     }
   });
 }
