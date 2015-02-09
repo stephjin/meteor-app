5b1d7b5470140ca20ea40ad53bc5debc6ea36255
stephjin <stephanie.k.jin@gmail.com>
2015-02-08 22:07:56 -0800

    Add update and remove function.

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -2,8 +2,8 @@
 // Client-side = creates a cache connected to server collection
 Tasks = new Mongo.Collection("tasks");
 
+// This code only runs on the client
 if (Meteor.isClient) {
-  // This code only runs on the client
 
   // Pass data into temapltes from JS by defining helpers
   Template.body.helpers({
@@ -16,7 +16,7 @@ if (Meteor.isClient) {
   // key = describe the event to listen for (listening for submit event on any element matching selector .new-task)
   // value = event handlers that are called when event happens
   Template.body.events({
-    "submit .new-task": function (event) {
+    "submit .new-task": function(event) {
       var text = event.target.text.value;
 
       Tasks.insert({
@@ -29,6 +29,18 @@ if (Meteor.isClient) {
 
       // Prevent default form submit
       return false;
+    }
+  });
+
+  // Logic for toggling checkbox
+  // This refers to individual task object
+  Template.task.events({
+    "click .toggle-checked": function() {
+      // 1st arg, selector that identifies subset of the collection. 2nd arg, update parameters that specifies what should be done to matched objects
+      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+    },
+    "click .delete": function() {
+      Tasks.remove(this._id);
     }
   });
 }
