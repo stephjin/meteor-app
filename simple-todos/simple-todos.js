7a9241aaa0b6ef66e10ada3ad0bdaa4890dc72db
stephjin <stephanie.k.jin@gmail.com>
2015-02-08 21:36:51 -0800

    Add task collection.

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -1,9 +1,12 @@
+// Server-side = sets up MongoDB collection
+// Client-side = creates a cache connected to server collection
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
+  // This code only runs on the client
   Template.body.helpers({
-    tasks: [
-    { text: "Task 1" },
-    { text: "Task 2" },
-    { text: "Task 3" }
-    ]
+    tasks: function () {
+      return Tasks.find({});
+    }
   });
 }
